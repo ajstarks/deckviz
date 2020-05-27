02c82205d5e0f29c1376672d28edf7a5419d6ef4
Anthony Starks <ajstarks@gmail.com>
2020-05-27 15:14:48 -0400

    add b17

---
+++ b17/kia.d
@@ -0,0 +1,9 @@
+Waist Gunner	18.5
+Tail Gunner	17.3
+Bombardier	16.4
+Navigator	13.2
+Top Turret	9.2
+Ball Turret	7.1
+Radio Op.	6.1
+Pilot	6.1
+Co-Pilot	6.1
---
+++ b17/wia.d
@@ -0,0 +1,9 @@
+Waist Gunner	22.2
+Bombardier	15.7
+Navigator	13.2
+Tail Gunner	12.0
+Radio Op.	9.0
+Top Turret	8.2
+Pilot	7.7
+Co-Pilot	6.7
+Ball Turret	5.3
