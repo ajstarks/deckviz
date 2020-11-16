386c6a31eacb7385e79adf7470678d7359933351
Anthony Starks <ajstarks@gmail.com>
2020-11-15 22:56:02 -0500

    add elections

---
+++ elections/2008pop.d
@@ -0,0 +1,51 @@
+Alabama	4718206
+Alaska	687455
+Arizona	6280362
+Arkansas	2874554
+California	36604337
+Colorado	4889730
+Connecticut	3545579
+Delaware	883874
+District of Columbia	580236
+Florida	18527305
+Georgia	9504843
+Hawaii	1332213
+Idaho	1534320
+Illinois	12747038
+Indiana	6424806
+Iowa	3016734
+Kansas	2808076
+Kentucky	4289878
+Louisiana	4435586
+Maine	1330509
+Maryland	5684965
+Massachusetts	6468967
+Michigan	9946889
+Minnesota	5247018
+Mississippi	2947806
+Missouri	5923916
+Montana	976415
+Nebraska	1796378
+Nevada	2653630
+New Hampshire	1315906
+New Jersey	8711090
+New Mexico	2010662
+New York	19212436
+North Carolina	9309449
+North Dakota	657569
+Ohio	11515391
+Oklahoma	3668976
+Oregon	3768748
+Pennsylvania	12612285
+Rhode Island	1055003
+South Carolina	4528996
+South Dakota	799124
+Tennessee	6247411
+Texas	24309039
+Utah	2663029
+Vermont	624151
+Virginia	7833496
+Washington	6562231
+West Virginia	1840310
+Wisconsin	5640996
+Wyoming	546043
