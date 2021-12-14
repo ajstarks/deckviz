4cccd6073e0b063462100d3ae63cb49b61cbd011
Anthony Starks <ajstarks@gmail.com>
2021-12-13 22:31:19 -0500

    add top-netflix

---
+++ top-netflix/most-watched.d
@@ -0,0 +1,16 @@
+# Netflix most watched series (views in the first 28 days)
+Squid Game (2021)	1650450000
+Bridgerton (2020)	625490000
+Money Heist Part 4 (2020)	619010000
+Stranger Things 3 (2019)	582100000
+The Witcher Season 1 (2019)	541010000
+13 Reasons Why Season 1 (2017)	475570000
+Maid (2021)	469090000
+You Season 3 (2021)	467830000
+Sex Education Season 3 (2021)	418760000
+Lupin Part 1 (2021)	316830000
+Elite, Season 3 (2020)	275300000
+Who Killed Sara? Season 1 (2021)	266430000
+Elite, Season 4 (2021)	257090000
+Lupin, Part 2 (2021)	214070000
+Dark Desire, Season 1 (2020)	213700000
