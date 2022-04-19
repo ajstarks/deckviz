f3dcf627240b521c2b9298d4da1a399ce3b811da
Anthony Starks <ajstarks@gmail.com>
2022-04-19 16:12:57 -0400

    human-freedom: add legend, average score

---
+++ human-freedom/avg.d
@@ -0,0 +1,10 @@
+North America	8.79
+Western Europe	8.72611
+Oceania	8.095
+East Asia	7.96333
+Eastern Europe	7.92455
+Latin America & the Caribbean	7.44346
+Caucasus & Central Asia	7.005
+South Asia	6.58706
+Sub-Saharan Africa	6.48444
+Middle East & North Africa	5.62579
