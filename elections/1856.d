f44cd62101e7f77a5e54c1ceb96abeb8cbcfb331
Anthony Starks <ajstarks@gmail.com>
2025-08-26 21:44:04 -0400

    update elections

---
+++ elections/1856.d
@@ -0,0 +1,57 @@
+State	1850
+Alabama	771623
+Alaska	
+American Samoa	
+Arizona	
+Arkansas	209897
+California	92597
+Colorado	
+Connecticut	370792
+Delaware	91532
+District of Columbia [e]	51687
+Florida	87445
+Georgia	906185
+Guam	
+Hawaii	
+Idaho	
+Illinois	851470
+Indiana	988416
+Iowa	192214
+Kansas	
+Kentucky	982405
+Louisiana [f]	517762
+Maine [g]	583169
+Maryland [h]	583034
+Massachusetts	994514
+Michigan	397654
+Minnesota	6077
+Mississippi	606526
+Missouri	682044
+Montana	
+Nebraska	
+Nevada	
+New Hampshire	317976
+New Jersey	489555
+New Mexico	61547
+New York	3097394
+North Carolina	869039
+North Dakota	
+Northern Mariana Islands	
+Ohio	1980329
+Oklahoma	
+Oregon	12093
+Pennsylvania	2311786
+Puerto Rico	
+Rhode Island	147545
+South Carolina	668507
+South Dakota [i]	
+Tennessee	1002717
+Texas	212592
+U.S. Virgin Islands	
+Utah	11380
+Vermont [j]	314120
+Virginia [k]	1119348
+Washington	1201
+West Virginia [l]	302313
+Wisconsin	305391
+Wyoming	
---
+++ elections/g.d
@@ -0,0 +1,52 @@
+	
+Nevada	1603163
+Arizona	4428068
+Utah	2000494
+Georgia	7353225
+Colorado	3822676
+Idaho	1189251
+Oregon	3203735
+Texas	19128261
+North Carolina	7322870
+Washington	5532939
+Florida	14399985
+New Mexico	1713407
+Tennessee	5319654
+New Hampshire	1162481
+Delaware	724842
+Montana	879372
+Arkansas	2509793
+California	31878234
+Minnesota	4657758
+Virginia	6675451
+South Carolina	3698746
+Oklahoma	3300902
+Nebraska	1652093
+Indiana	5840528
+Alaska	607007
+Missouri	5358692
+Mississippi	2716115
+Wisconsin	5159795
+Kentucky	3883723
+Vermont	588654
+Maryland	5071604
+Alabama	4273084
+Michigan	9594350
+New Jersey	7987933
+Illinois	11846544
+Wyoming	481400
+South Dakota	732405
+Hawaii	1183723
+Maine	1243316
+Massachusetts	6092352
+Ohio	11172782
+Kansas	2572150
+North Dakota	643539
+Iowa	2851792
+Louisiana	4350579
+Connecticut	3274238
+West Virginia	1825754
+New York	18184774
+Pennsylvania	12056112
+Rhode Island	990225
+District of Columbia	543213
