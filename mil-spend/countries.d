f544569b6139abce1d62294783b09783871fa61b
Anthony Starks <ajstarks@gmail.com>
2026-06-16 12:14:39 -0400

    mil-spend: use countries.d

---
+++ mil-spend/countries.d
@@ -0,0 +1,10 @@
+geo:39,-98	United\nStates
+geo:66.755,93.547859Russia
+geo:35,103	China
+geo:21.918297,78.01215	India
+geo:54.003644,-2.547859	United Kingdom
+geo:24,45	Saudi\nArabia
+geo:46.8194,2.4561	France
+geo:51,9	Germany
+geo:36,138	Japan
+geo:49.032,31.505	Ukraine
