a720c53a3c54d4d09850524f86fe02d6f85517bd
Anthony Starks <ajstarks@gmail.com>
2026-06-16 00:00:41 -0400

    add mil-spend

---
+++ mil-spend/mil-spend.d
@@ -0,0 +1,11 @@
+# Highest Military Spending (Billion USD)
+United States	916
+China	296
+Russia	109
+India	83.6
+Saudi Arabia	75.8
+United Kingdom	74.9
+Germany	66.8
+Ukraine	64.8
+France	61.3
+Japan	50.2
