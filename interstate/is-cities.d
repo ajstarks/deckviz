e0e216dbb3c2adeda3be7c225dcfbcaa741aef9e
Anthony Starks <ajstarks@gmail.com>
2026-06-15 23:59:55 -0400

    add Interstate Highway

---
+++ interstate/is-cities.d
@@ -0,0 +1,45 @@
+geo:33.7490,-84.3880	Atlanta
+geo:41.8781,-87.6298	Chicago
+geo:32.7767,-96.7970	Dallas\nFort Worth
+geo:39.7392,-104.9903	Denver
+geo:29.7604,-95.3698	Houston
+geo:39.7684,-86.1581	Indianapolis
+geo:39.0997,-94.5786	Kansas City
+geo:34.0522,-118.2437	Los Angeles
+geo:44.9778,-93.2650	Minneapolis
+geo:36.1627,-86.7816	Nashville
+geo:38.6270,-90.1994	St. Louis
+geo:37.7749,-122.4194	San Francisco
+geo:38.89511,-77.03637	Wash. DC
+geo:40.71278,-74.00597	New York
+#geo:39.96118,-82.99880	Columbus
+geo:37.5407,-77.4360	Richmond
+geo:35.2271,-80.8431	Charlotte
+geo:39.103119,-84.512016	Cincinnati
+geo:46.0039,-112.5328	Butte
+geo:40.7608,-111.8910	Salt Lake City
+geo:33.4484,-112.0740	Phoenix
+geo:29.4241,-98.493	San Antonio
+geo:42.3314,-83.0458	Detroit
+geo:25.7617,-80.1918	Miami
+geo:35.0844,-106.6504	Albuquerque
+geo:38.2561,-85.7516	Louisville
+geo:45.787636,-108.489304	Billings
+geo:35.481918,-97.508469	Oklahoma City
+geo:35.149349,-90.057617	Memphis
+geo:47.608013,-122.335167	Seattle
+geo:45.523064,-122.676483	Portland
+geo:38.575764,-121.478851	Sacramento
+geo:42.361145,-71.057083	Boston
+geo:34.895798,-117.017281	Barstow
+geo:35.198284,-111.651299	Flagstaff
+geo:39.952,-75.165	Philadelphia
+geo:30.332,-81.656	Jacksonville
+geo:30.471,-91.147	Baton Rouge
+#geo:32.52366,-93.76350	Shreveport
+geo:34.7465,-92.2896	Little Rock
+geo:32.2535,-110.9118	Tucson
+geo:32.32659,-106.77544	Las Cruces
+
+
+
